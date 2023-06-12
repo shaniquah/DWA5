61504795de311415085583e29b173e40f851be1e
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-06-12 20:59:57 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,34 @@ const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  try {
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+    result.innerText = Math.floor(dividend / divider);
+    
+    if (!dividend || !divider) {
+      result.innerText = 'Division not performed. Both values are required in inputs. Try again'
+    }
+
+    if (result.innerText < 0) {
+      result.innerText = 'Division not performed. Invalid number provided. Try again'
+      throw new Error('Invalid number provided');
+    };
+
+    if ((typeof dividend !== Number && dividend !== null) 
+    || (typeof divider !== Number && divider !== null)) {
+      throw new Error('No number detected')
+      document.body.innerHTML = '<h1 style="color: red">Something critical went wrong. Please reload the page</h1>'
+    }
+
+  } catch (error) {
+      console.error(error);  
+
+      if (!event.defaultPrevented) {
+        event.preventDefault();
+      }
+      throw error;
+  };
 });
+
